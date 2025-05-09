caa1cf9b68c79b7ed1383155766bfea446b78217
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-09 17:31:35 +0000

    variables

---
+++ 01_basics/varibles.js
@@ -0,0 +1,16 @@
+const accountNum = 12345;
+let accountName = "saving";
+var accountPass = "12343458";
+cityName="patna";
+
+// accountNum = 383;
+
+accountName = "current";
+accountPass = "09876";
+cityName = "Delhi";
+
+// console.log(accountName);
+
+
+console.table([accountName, accountNum, accountPass, cityName]);
+
