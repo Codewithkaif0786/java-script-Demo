90e722d27a04518afc1d8ddfcff609232429639d
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-09 18:56:51 +0000

    conversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,15 @@
+// let value = 3;
+
+// let newValue = -value;
+// console.log(newValue);
+
+// let str = "kaif";
+// let str1 = " ali";
+// let add = str + str1;
+// console.log(add);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+
