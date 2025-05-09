fb460e1a825e4085c6a7db1de4db88458253e992
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-09 17:42:48 +0000

    data types

---
+++ 01_basics/dataType.js
@@ -0,0 +1,15 @@
+//Primitives
+
+let a = 10; // number type
+let name = "kaif"; // string
+let isStudent = true // boolean
+let score;  // undefined
+let empty = null;  // null
+let big = 3883389483449849393484n; // bigInt
+let unique = Symbol("id"); // symbol
+
+
+//non Primitives
+let person = { name: "Kaif", age: 22 };  // Object
+let marks = [90, 85, 88];                // Array
+function greet() { return "Hello"; }     // Function
