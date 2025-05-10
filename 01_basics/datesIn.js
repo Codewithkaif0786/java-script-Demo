a3b338712eaa95d79e57582f6b4f4f6add6b50b9
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-10 18:29:55 +0000

    date

---
+++ 01_basics/datesIn.js
@@ -0,0 +1,7 @@
+// let myDate = new Date();
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2025, 0 , 23, 5, 3)
+console.log(myCreatedDate.toLocaleString());
