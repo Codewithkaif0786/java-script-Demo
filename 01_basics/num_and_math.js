54c77a222ca9a23a1c6298a9b5b0e34d71cd318e
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-10 17:10:18 +0000

    num_and_math

---
+++ 01_basics/num_and_math.js
@@ -0,0 +1,20 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const hundreads = 1000000;
+// console.log(hundreads.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.floor(4.6));
+console.log(Math.ceil(4.5));
+console.log(Math.round(4.5));
+
