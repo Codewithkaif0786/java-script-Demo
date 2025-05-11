86d3abb4049c52689c6b2c4f9955c0587cb9a3a0
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-11 16:31:06 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+// const arr = [1, 3, 4, 5, 8];
+// console.log(arr[0]);
+
+
+// const myHeros = ["kaif", "saif", "arbaz", "kais"];
+// myHeros.push("hera");
+// myHeros.pop("hera");
+// console.log(myHeros);
+
+
+
