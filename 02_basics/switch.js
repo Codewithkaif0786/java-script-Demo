a1e99ff9bc06002a4e247c7d08bc2128f91cef6f
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-13 18:10:38 +0000

    switch

---
+++ 02_basics/switch.js
@@ -0,0 +1,26 @@
+let month = 5;
+
+switch (month) {
+    case 1:
+        console.log("janauary");
+        break;
+    case 2:
+        console.log("febrauary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("arpril");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+
+    default:
+        console.log("invalid");
+        break;
+}
