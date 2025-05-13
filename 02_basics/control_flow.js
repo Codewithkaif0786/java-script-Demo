770e48affd91008d364751257df23be09efbb82b
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-13 18:02:59 +0000

    control flow

---
+++ 02_basics/control_flow.js
@@ -0,0 +1,35 @@
+// if(2 === "2") {
+//     console.log("executed");
+// }else {
+//     console.log("not executed");
+// }
+
+// let score = 200;
+// if(score > 100) {
+//     let power = "fly";
+//     console.log(`User Power: ${power}`);
+// } 
+
+// let balance = 1000;
+//  if(balance > 500) console.log("pass");
+ 
+
+// const userLoggedIn = true;
+// const debitCard = true;
+
+// if(userLoggedIn && debitCard) {
+//     console.log("user allow to shopping");
+    
+// } else {
+//     console.log("not allow to shopping");
+    
+// }
+
+
+const userLoggedInEmail = true;
+const userLoggedInGoogle = true;
+
+if(userLoggedInEmail || userLoggedInGoogle) {
+    console.log("loging successfully!");
+    
+}
