a27399ac275ebd7583eebd025a0e02409b228e38
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-13 08:32:30 +0000

    scope

---
+++ 02_basics/scope.js
@@ -0,0 +1,48 @@
+if(true) {
+    let a = 10;
+    const b= 20;
+    var c = 30;
+}
+// console.log(c);
+// console.log(b);
+// console.log(c);
+
+// function one() {
+//     const userName = "kaif";
+//     function two() {
+//         const sweetName = "hera";
+//         console.log(userName);
+//     }
+//     two();
+// }
+// one();
+
+if(true) {
+    const userName = "kaif";
+    if(userName === "kaif") {
+        const website = " youtube";
+        console.log(userName + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(userName);
+
+
+//+=+++++============
+// interesting cocept
+
+console.log(one(5));
+function one (num) {
+    return num+1;
+}
+
+
+
+// console.log(addTwo(2));
+// const addTwo = function(num2) {
+//     return num2+2;
+// }
+
+
