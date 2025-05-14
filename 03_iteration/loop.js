8b878ccbc2e5496b27da338bcb92e9ae9e1e95fa
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-14 05:40:08 +0000

    loop

---
+++ 03_iteration/loop.js
@@ -0,0 +1,61 @@
+// for(let i=1; i<=10; i++) {
+//     console.log(i + " ");
+    
+// }
+
+// let mul = 2;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`table ${mul * i}`);
+// }
+
+// let myArray = ["kaif", "saif", "arbaz", "kais"];
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+// for (let index = 1; index < 20; index++) {
+//     if(index === 5) {
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+    
+// }
+
+
+// for (let index = 1; index < 20; index++) {
+//     if(index === 5) {
+//         continue;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+    
+// }
+
+
+// let num = 1;
+
+// while(num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+
+// let myArray = ["kaif", "hera", "kuzzu", "kais"];
+// let arr = 0;
+// while(arr < myArray.length) {
+//     console.log(myArray[arr]);
+//     arr++;
+// }
+
+
+let score = 11;
+do {
+    console.log(`score is ${score}`);
+    score++;
+    
+} while (score <= 10);
